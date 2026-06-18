48a0ea40777bf670fa43d741ddc6f0d882f0713f
p.gabriele <g.prestano@general-group.it>
2026-06-18 16:51:22 +0200

    feat(tickets): implement automatic navigation to first ticket on initialization

--- src/app/features/tickets/tickets.ts
+++ src/app/features/tickets/tickets.ts
@@ -1,9 +1,9 @@
-import { Component, computed, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, computed, effect, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { Container } from '../../layout/container/container';
 import { TicketsService } from '../../services/tickets.service';
 import { CommonModule } from '@angular/common';
 import { TicketStatus } from '../../models/types/tickets.types';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { Header } from '../../core/header/header';
 import { TicketCard } from '../../shared/ticket-card/ticket-card';
 import { TicketSkeletonCard } from '../../shared/ticket-skeleton-card/ticket-skeleton-card';
@@ -25,7 +25,9 @@ import { TicketSkeletonDetail } from '../../shared/ticket-skeleton-detail/ticket
   styleUrl: './tickets.css',
 })
 export class Tickets implements OnInit, OnDestroy {
-  private ticketsService = inject(TicketsService);
+  protected ticketsService = inject(TicketsService);
+  protected router = inject(Router);
+  protected route = inject(ActivatedRoute);
 
   tickets = this.ticketsService.filteredTickets;
   isLoading = this.ticketsService.isLoadingTickets;
@@ -45,6 +47,23 @@ export class Tickets implements OnInit, OnDestroy {
     const totalTickets = this.tickets().length;
     return Math.ceil(totalTickets / this.TICKETS_PER_PAGE);
   });
+
+  // Effect to handle automatic navigation to the first ticket when the component is initialized and there are tickets available, but no specific ticket is selected yet.
+  constructor() {
+    effect(() => {
+      const ticketsList = this.ticketsService.tickets()?.sort((a, b) => new Date(b.dataCreazione).getTime() - new Date(a.dataCreazione).getTime());
+      const currentTicketID = this.ticketsService.ticketId();
+
+      if (ticketsList && ticketsList.length > 0 && !currentTicketID) {
+        const firstTicketID = ticketsList[0]._id;
+
+        this.router.navigate([`ticket/${firstTicketID}`], {
+          relativeTo: this.route,
+          replaceUrl: true
+        });
+      }
+    });
+  }
 
   goToPreviousPage() {
     if (this.currentPage() > 1) {
